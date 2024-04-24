521f06f3412cfe326dad5c028d34064901be74cd
Franky Wijanarko <frank.jr.619@gmail.com>
2024-04-24 18:23:20 +0700

    setup ApiResponse

---
+++ composeApp/src/commonMain/kotlin/com/fjr619/currencykmmcompose/data/remote/model/ApiResponse.kt
@@ -0,0 +1,23 @@
+package com.fjr619.currencykmmcompose.data.remote.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ApiResponse(
+    val meta: MetaData,
+    val data: Map<String, Currency>
+)
+
+@Serializable
+data class MetaData(
+    @SerialName("last_updated_at")
+    val lastUpdatedAt: String
+)
+
+@Serializable
+data class Currency (
+    val code: String,
+    var value: Double
+)
+
